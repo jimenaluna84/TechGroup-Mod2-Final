0bc4f0bcbb2113e79f13e6ea35a3a49232a6fb71
PC4 <jimenaluna84@gmail.com>
2020-07-31 13:03:47 -0400

    Refactor Classes

---
+++ src/lunamary/modelPerson/Parent.java
@@ -0,0 +1,12 @@
+package lunamary.modelPerson;
+
+import java.util.List;
+
+public class Parent extends Person{
+
+    public Parent(String name, String lastName, int age, List<Device> deviceList) {
+        super(name, lastName, age, deviceList);
+
+    }
+
+}

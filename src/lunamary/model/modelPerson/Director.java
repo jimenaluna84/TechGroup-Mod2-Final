791988e34e90515de93cea0a38fe6e3dc1a6b0ba
PC4 <jimenaluna84@gmail.com>
2020-08-11 21:11:06 -0400

    Updated  class menu

--- src/lunamary/model/modelPerson/Director.java
+++ src/lunamary/model/modelPerson/Director.java
@@ -2,8 +2,8 @@ package lunamary.model.modelPerson;
 
 public class Director extends AbstractPerson {
 
-    public Director(String name, String lastName, int age) {
-        super(name, lastName, age);
+    public Director(String name, String lastName, int age,String gender) {
+        super(name, lastName, age,gender);
 
     }
 
